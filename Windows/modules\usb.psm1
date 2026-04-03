8c688f988704eeb3aea04aff002e7ec79fe6d530
BOUCHERET <webmasterdu63@gmail.com>
2026-04-03 11:02:22 +0200

    Add Get-USBDrives function to retrieve USB drives

--- Windows/modules\usb.psm1
+++ Windows/modules\usb.psm1
@@ -1,1 +1,8 @@
 
+function Get-USBDrives {
+    Get-WmiObject Win32_DiskDrive |
+        Where-Object { $_.InterfaceType -eq "USB" } |
+        Select-Object Model, DeviceID
+}
+
+Export-ModuleMember -Function Get-USBDrives
