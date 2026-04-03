8b42fd5f5c1c8b944be4435b0d77d6d04397f181
BOUCHERET <webmasterdu63@gmail.com>
2026-04-03 11:41:58 +0200

    Update modules\usb.psm1

--- Windows/modules\usb.psm1
+++ Windows/modules\usb.psm1
@@ -1,3 +1,9 @@
+function Get-USBDrives {
+    Get-WmiObject Win32_DiskDrive |
+        Where-Object { $_.InterfaceType -eq "USB" } |
+        Select-Object Model, DeviceID
+}
+
 function Format-USBDrive {
     param(
         [string]$DeviceID,
@@ -24,4 +30,5 @@ exit
 
     Remove-Item $scriptPath -Force
 }
+
 Export-ModuleMember -Function Get-USBDrives, Format-USBDrive
