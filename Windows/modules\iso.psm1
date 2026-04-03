a24dc3e59c567be808157ae59014a398e2e3a49e
BOUCHERET <webmasterdu63@gmail.com>
2026-04-03 11:02:00 +0200

    Add ISO selection and hash functions
    Added functions to select a Linux ISO file and get its SHA256 hash.

--- Windows/modules\iso.psm1
+++ Windows/modules\iso.psm1
@@ -1,1 +1,27 @@
 
+Add-Type -AssemblyName System.Windows.Forms
+
+function Select-LinuxISO {
+    $dialog = New-Object System.Windows.Forms.OpenFileDialog
+    $dialog.Filter = "Fichiers ISO (*.iso)|*.iso"
+    $dialog.Title  = "Sélectionner une ISO Linux"
+
+    if ($dialog.ShowDialog() -eq "OK") {
+        return $dialog.FileName
+    }
+
+    return $null
+}
+
+function Get-ISOHash {
+    param([string]$ISOPath)
+
+    if (!(Test-Path $ISOPath)) {
+        return "ISO introuvable."
+    }
+
+    $hash = Get-FileHash -Path $ISOPath -Algorithm SHA256
+    return $hash.Hash
+}
+
+Export-ModuleMember -Function Select-LinuxISO, Get-ISOHash
