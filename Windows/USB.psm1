52f87d17819b1ded1fba6428531112afa922cb78
BOUCHERET <webmasterdu63@gmail.com>
2026-04-03 14:04:59 +0200

    Create USB.psm1

---
+++ Windows/USB.psm1
@@ -0,0 +1,34 @@
+function Get-USBDrives {
+    Get-WmiObject Win32_DiskDrive |
+        Where-Object { $_.InterfaceType -eq "USB" } |
+        Select-Object Model, DeviceID
+}
+
+function Format-USBDrive {
+    param(
+        [string]$DeviceID,
+        [scriptblock]$Logger
+    )
+
+    $diskNumber = ($DeviceID -replace "[^\d]").Trim()
+
+    & $Logger "Formatage de la clé USB..."
+
+    $script = @"
+select disk $diskNumber
+clean
+create partition primary
+format fs=fat32 quick
+assign
+exit
+"@
+
+    $scriptPath = "$env:TEMP\diskpart_script.txt"
+    $script | Out-File -FilePath $scriptPath -Encoding ASCII
+
+    diskpart /s $scriptPath | Out-Null
+
+    Remove-Item $scriptPath -Force
+}
+
+Export-ModuleMember -Function Get-USBDrives, Format-USBDrive
