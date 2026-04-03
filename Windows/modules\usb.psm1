7d0934c3b831904d4877821781b9f9d64327adbb
BOUCHERET <webmasterdu63@gmail.com>
2026-04-03 11:29:06 +0200

    Update modules\usb.psm1

--- Windows/modules\usb.psm1
+++ Windows/modules\usb.psm1
@@ -6,3 +6,25 @@ function Get-USBDrives {
 }
 
 Export-ModuleMember -Function Get-USBDrives
+function Format-USBDrive {
+    param([string]$DeviceID)
+
+    $diskNumber = ($DeviceID -replace "[^\d]").Trim()
+
+    $script = @"
+select disk $diskNumber
+clean
+create partition primary
+format fs=fat32 quick
+assign
+exit
+"@
+
+    $scriptPath = "$env:TEMP\diskpart_script.txt"
+    $script | Out-File -FilePath $scriptPath -Encoding ASCII
+
+    Write-Log "Formatage de la clé USB..."
+    diskpart /s $scriptPath | Out-Null
+
+    Remove-Item $scriptPath -Force
+}
